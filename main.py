7f94f1cac606e7c796bfa6d5fafb6356461e76c1
natividadlolhaha <endlessparadox2010@gmail.com>
2026-04-08 07:43:02 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from pyscript import document
+import numpy as np
+import matplotlib.pyplot as plt
+
+def sample_numpy(event):
+    document.getElementById('output').innerHTML = " "
+
+    days = np.array(['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'])
+    nuggetseaten = np.array([10, 5, 8, 12, 20, 15, 6])
+
+    plt.plot(days, nuggetseaten)
+    plt.title("Chicken Nuggets Eaten Per Day")
+
+    plt.show()
